05af9389c17c30bc9273ff8f78aea7de81ec513a
bekalshenoy <bekalshenoy@gmail.com>
2021-08-31 15:29:58 +0530

    update

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
